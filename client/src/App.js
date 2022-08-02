669d630e2a905f042d903d18444ac00e24b7011d
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-03 02:15:03 +0700

    xong phan1 dk dn

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,72 @@
 import './App.css';
-
+import { useState } from 'react';
+import axios from 'axios';
 function App() {
+    const registerData = {
+        username: '',
+        password: '',
+    };
+    const loginData = {
+        usernameLogin: '',
+        passwordLogin: '',
+    };
+    const [register, setRegister] = useState({ ...registerData });
+    const [login, setLogin] = useState({ ...loginData });
+    const [statusLogin, setStatusLogin] = useState('');
+
+    const handleChange = (e) => {
+        setRegister((p) => {
+            // console.log('-----', { ...p });
+            // console.log('++++++', { ...p, [e.target.name]: e.target.value });
+            return { ...p, [e.target.name]: e.target.value };
+        });
+    };
+    const handleRegister = () => {
+        axios.post('http://localhost:3001/register', { ...register }).then((response) => {
+            console.log('Dang ky thanh cong ', response);
+        });
+    };
+
+    const handleChangeLogin = (e) => {
+        setLogin((p) => {
+            // console.log('Dang', { ...p, [e.target.name]: e.target.value });
+            return { ...p, [e.target.name]: e.target.value };
+        });
+    };
+    // console.log(login);
+    const handleLogin = () => {
+        axios.post('http://localhost:3001/login', { ...login }).then((response) => {
+            console.log('Dang nhap thanh cong  ', response);
+            let data = response.data;
+            if (data.message) {
+                setStatusLogin(data.message);
+            } else {
+                setStatusLogin(data[0].username);
+            }
+        });
+    };
+
     return (
         <div className="App">
             <div className="register">
                 <h1>Register</h1>
                 <label>Username</label>
-                <input type="text" />
+                <input type="text" name="username" onChange={handleChange} />
                 <label>Password</label>
-                <input type="password" />
-                <button type="submit">Register</button>
+                <input type="text" name="password" onChange={handleChange} />
+                <button type="submit" onClick={handleRegister}>
+                    Register
+                </button>
             </div>
             <div className="login">
                 <h1>Login</h1>
-                <input type="text" />
-                <input type="password" />
-                <button type="submit">Login</button>
+                <input type="text" name="usernameLogin" onChange={handleChangeLogin} />
+                <input type="text" name="passwordLogin" onChange={handleChangeLogin} />
+                <button type="submit" onClick={handleLogin}>
+                    Login
+                </button>
             </div>
+            <h1>{statusLogin}</h1>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -12,7 +12,37 @@ const db = mysql.createConnection({
     database: 'sern3',
     port: 3307,
 });
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    db.query('INSERT INTO new_table (username, password) VALUES (?,?)', [username, password], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.usernameLogin;
+    const password = req.body.passwordLogin;
+
+    db.query('SELECT * FROM new_table WHERE username = ? AND password = ?', [username, password], (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        }
+        // console.log(typeOf.result);
+        if (result) {
+            res.send(result);
+        } else {
+            res.send({
+                message: 'Ko tim thay nguoi dung',
+            });
+        }
+    });
+});
 app.listen(3001, () => {
     console.log('Hello your server is running on port 3001');
 });
