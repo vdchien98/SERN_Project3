eafa3b01c4e5069132e3b05138a469624f819996
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-02 17:44:43 +0700

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Hello World , Xin chao </h1>
+            <div className="register">
+                <h1>Register</h1>
+                <label>Username</label>
+                <input type="text" />
+                <label>Password</label>
+                <input type="password" />
+                <button type="submit">Register</button>
+            </div>
+            <div className="login">
+                <h1>Login</h1>
+                <input type="text" />
+                <input type="password" />
+                <button type="submit">Login</button>
+            </div>
         </div>
     );
 }
